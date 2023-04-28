0d905ddaae9ba0eee9552105d026a20be86f4be8
haeyeon0106 <yeon99016@gmail.com>
2023-04-28 14:08:07 +0900

    album domain 수정 및 photo domain 생성

---
+++ src/main/java/com/example/photoalbum/domain/Photo.java
@@ -0,0 +1,33 @@
+package com.example.photoalbum.domain;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "photo",schema = "photo_album",uniqueConstraints = {@UniqueConstraint(columnNames = "photo_id")})
+public class Photo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "photo_id",unique = true,nullable = false)
+    private Long photoId;
+
+    @Column(name = "file_name",nullable = false)
+    private String fileName;
+
+    @Column(name = "thumb_url",nullable = false)
+    private String thumbUrl;
+
+    @Column(name = "original_url",nullable = false)
+    private String originalUrl;
+
+    @Column(name = "file_size",nullable = false)
+    private int fileSize;
+
+    @Column(name = "uploaded_at",nullable = false)
+    private Date uploadedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "album_id")
+    private Album album;
+}
